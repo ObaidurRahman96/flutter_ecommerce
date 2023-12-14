c58463aa4522869177c10f7270bb0e2bf47f137f
ObaidurRahman96 <obaidur.cse.pstu@gmail.com>
2023-12-15 01:54:01 +0600

    This is my first commit

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,3 +1,4 @@
+import 'package:e_commerce/Home/main_foodpage.dart';
 import 'package:e_commerce/homepage.dart';
 import 'package:flutter/material.dart';
 
@@ -8,12 +9,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SafeArea(
+        child:MainFoodPage(),
+      ),
     );
   }
 }
